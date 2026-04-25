82e49a2c33b407799d9126536fc4cdcdd6123736
Giuliano Cardoso <giulianocf@gmail.com>
2026-04-25 13:10:21 -0500

    feat: add ScorePanel, TileWidget, and BoardWidget

---
+++ lib/presentation/widgets/board_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../core/constants/game_constants.dart';
+import '../controllers/game_notifier.dart';
+import 'tile_widget.dart';
+
+class BoardWidget extends ConsumerWidget {
+  const BoardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final board = ref.watch(gameProvider).board;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final boardSize = screenWidth - GameConstants.boardPadding * 2;
+    final tileSize = (boardSize - GameConstants.tileSpacing * (GameConstants.boardSize + 1)) /
+        GameConstants.boardSize;
+
+    return Container(
+      width: boardSize,
+      height: boardSize,
+      decoration: BoxDecoration(
+        color: const Color(0xFFE8D5B7),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      padding: const EdgeInsets.all(GameConstants.tileSpacing),
+      child: Column(
+        children: List.generate(GameConstants.boardSize, (r) =>
+          Expanded(
+            child: Row(
+              children: List.generate(GameConstants.boardSize, (c) =>
+                Padding(
+                  padding: const EdgeInsets.all(GameConstants.tileSpacing / 2),
+                  child: TileWidget(tile: board[r][c], size: tileSize),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/widgets/tile_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import '../../data/animals_data.dart';
+import '../../data/models/tile.dart';
+
+class TileWidget extends StatelessWidget {
+  final Tile? tile;
+  final double size;
+
+  const TileWidget({super.key, required this.tile, required this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    if (tile == null) {
+      return Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          color: const Color(0xFFC9B79C),
+          borderRadius: BorderRadius.circular(8),
+        ),
+      );
+    }
+
+    final animal = animalForLevel(tile!.level);
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        color: animal.tileColor,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: const [
+          BoxShadow(color: Colors.black26, blurRadius: 4, offset: Offset(0, 2)),
+        ],
+      ),
+      child: Center(
+        child: Text(
+          '${tile!.level}',
+          style: TextStyle(
+            fontSize: size * 0.35,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            shadows: const [Shadow(color: Colors.black38, blurRadius: 2)],
+          ),
+        ),
+      ),
+    );
+  }
+}
