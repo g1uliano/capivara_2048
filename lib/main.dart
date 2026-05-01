808e083d889932d48c4f8b353877bb1214009d2d
Giuliano Cardoso <giulianocf@gmail.com>
2026-05-01 12:49:03 -0500

    feat: registrar DailyRewardsStateAdapter e inicializar provider em main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'data/models/lives_state_adapter.dart';
 import 'data/models/inventory_hive_adapter.dart';
+import 'data/models/daily_rewards_state_adapter.dart';
 import 'core/providers/reduce_effects_provider.dart';
 import 'domain/inventory/inventory_notifier.dart';
+import 'domain/daily_rewards/daily_rewards_notifier.dart';
 import 'app.dart';
 
 void main() async {
@@ -12,8 +14,11 @@ void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(LivesStateAdapter());
   Hive.registerAdapter(InventoryHiveAdapter());
+  Hive.registerAdapter(DailyRewardsStateAdapter());
   final container = ProviderContainer();
   await container.read(reduceEffectsProvider.notifier).load();
   await container.read(inventoryProvider.notifier).load();
+  await container.read(dailyRewardsProvider.notifier).load();
   runApp(UncontrolledProviderScope(container: container, child: const CapivaraApp()));
 }
+
