729f2dab1a8cee05d3ea6e6cc9c27b7ce39ee21d
Giuliano Cardoso <giulianocf@gmail.com>
2026-05-07 06:56:10 -0500

    refactor(riverpod): migrate DailyRewardsNotifier → Notifier

--- lib/domain/daily_rewards/daily_rewards_notifier.dart
+++ lib/domain/daily_rewards/daily_rewards_notifier.dart
@@ -6,14 +6,12 @@ import '../inventory/inventory_notifier.dart';
 import '../lives/lives_notifier.dart';
 import 'daily_rewards_engine.dart';
 
-class DailyRewardsNotifier extends StateNotifier<DailyRewardsState> {
-  DailyRewardsNotifier(this._repo, this._ref) : super(DailyRewardsState.initial());
-
-  final DailyRewardsRepository _repo;
-  final Ref _ref;
+class DailyRewardsNotifier extends Notifier<DailyRewardsState> {
+  @override
+  DailyRewardsState build() => DailyRewardsState.initial();
 
   Future<void> load() async {
-    state = await _repo.load();
+    state = await ref.read(dailyRewardsRepositoryProvider).load();
   }
 
   DailyRewardStatus get status => computeDailyRewardStatus(DateTime.now(), state);
@@ -32,19 +30,19 @@ class DailyRewardsNotifier extends StateNotifier<DailyRewardsState> {
 
     final reward = rewardForDay(current.currentDay);
 
-    if (reward.lives > 0) await _ref.read(livesProvider.notifier).addEarned(reward.lives);
-    if (reward.undo1 > 0) await _ref.read(inventoryProvider.notifier).add(ItemType.undo1, reward.undo1);
-    if (reward.bomb2 > 0) await _ref.read(inventoryProvider.notifier).add(ItemType.bomb2, reward.bomb2);
+    if (reward.lives > 0) await ref.read(livesProvider.notifier).addEarned(reward.lives);
+    if (reward.undo1 > 0) await ref.read(inventoryProvider.notifier).add(ItemType.undo1, reward.undo1);
+    if (reward.bomb2 > 0) await ref.read(inventoryProvider.notifier).add(ItemType.bomb2, reward.bomb2);
 
     final next = applyClaim(now, current);
     state = next;
-    await _repo.save(state);
+    await ref.read(dailyRewardsRepositoryProvider).save(state);
   }
 
   Future<void> claimDouble(DailyReward original) async {
-    if (original.lives > 0) await _ref.read(livesProvider.notifier).addEarned(original.lives);
-    if (original.undo1 > 0) await _ref.read(inventoryProvider.notifier).add(ItemType.undo1, original.undo1);
-    if (original.bomb2 > 0) await _ref.read(inventoryProvider.notifier).add(ItemType.bomb2, original.bomb2);
+    if (original.lives > 0) await ref.read(livesProvider.notifier).addEarned(original.lives);
+    if (original.undo1 > 0) await ref.read(inventoryProvider.notifier).add(ItemType.undo1, original.undo1);
+    if (original.bomb2 > 0) await ref.read(inventoryProvider.notifier).add(ItemType.bomb2, original.bomb2);
   }
 
   void debugSetState(DailyRewardsState s) => state = s;
@@ -54,6 +52,6 @@ final dailyRewardsRepositoryProvider = Provider<DailyRewardsRepository>(
   (_) => DailyRewardsRepository(),
 );
 
-final dailyRewardsProvider = StateNotifierProvider<DailyRewardsNotifier, DailyRewardsState>(
-  (ref) => DailyRewardsNotifier(ref.read(dailyRewardsRepositoryProvider), ref),
+final dailyRewardsProvider = NotifierProvider<DailyRewardsNotifier, DailyRewardsState>(
+  DailyRewardsNotifier.new,
 );
